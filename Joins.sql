888d23fe2465ed109aece85d5a9804c92058f536
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-01 12:43:29 +0530

    Joins

---
+++ Joins.sql
@@ -0,0 +1,38 @@
+USE employees;
+
+CREATE TABLE role (
+id int primary key auto_increment,
+roleName enum("Batsman","Bowler","All rounder") not null,
+playerId int not null
+);
+
+select * from cricketer;
+
+INSERT INTO role (roleName,playerId) values 
+("All rounder",1),
+("Batsman",2),
+("Batsman",6),
+("Batsman",8),
+("Bowler",7),
+("All rounder",20);
+
+select * from role;
+
+SELECT c.name,r.roleName 
+FROM cricketer c INNER JOIN role r ON c.id=r.playerId;
+
+SELECT c.name,r.roleName 
+FROM cricketer c LEFT JOIN role r ON c.id=r.playerId
+
+Union
+
+SELECT c.name,r.roleName 
+FROM cricketer c RIGHT JOIN role r ON c.id=r.playerId;
+
+SELECT concat("He is ",upper(c.name),". he plays as a ",upper(r.roleName)
+,". for ",upper(co.countryName),".") 
+as description from cricketer c inner join 
+country co on c.id=co.playerId
+inner join role r on r.playerId=c.id;
+
+SELECT * from cricketer CROSS JOIN role;
