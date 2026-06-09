5f39f6021d0afbd33971fced7d7c8084d6436fe8
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-09 12:30:46 +0530

    Transactions

---
+++ Where.sql
@@ -0,0 +1,21 @@
+USE junebatch;
+
+SELECT * FROM actordetails;
+
+
+Insert into actordetails value
+(1,"Salman khan",44),
+(2,"Shahrukh khan",50),
+(3,"Aamir khan",56),
+(4,"Ranbir kapoor",40),
+(5,"Ranveer singh",38),
+(6,"Hrithik roshan",48),
+(7,"Ajay devgan",52),
+(8,"Akshay kumar",56),
+(9,"Varun dhawan",32),
+(10,"Sidharth malhotra",34),
+(11,"Vicky kaushal",35),
+(12,"Ayushmann khurrana",39),
+(13,"Rajkumar rao",38),
+(14,"Nawazuddin siddiqui",49),
+(15,"Irrfan khan",53);
